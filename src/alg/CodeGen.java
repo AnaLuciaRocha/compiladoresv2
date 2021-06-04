3249bc2ecf43426d895b7d8aabc1f30920a846b2
Jay <jcrmorelli@gmail.com>
2021-06-04 15:16:59 +0100

    jay push

--- src/alg/CodeGen.java
+++ src/alg/CodeGen.java
@@ -82,18 +82,9 @@ public class CodeGen extends algBaseVisitor<Symbol>{
         return null;
     }
 
-    //variable declarations and initialization
-    //v_decl --> type IDENT EQUAL (exp | LRECT exp RRECT) #VarDeclInit;
-    //we don't need to do anything for the variable declaration, but we do have to do for initialization
-    //Task T1
-    public Symbol visitVarDeclInit(alg.VarDeclInitContext ctx)
-    {
-        //TODO
-    }
-
     //simple expressions
     //simple_exp --> LINT;
-    public Symbol visitLInt(alg.LIntContext ctx)
+    public Symbol visitInt(alg.IntContext ctx)
     {
         Symbol s = new Symbol(new Type(Type.PType.INT),ctx.getText(), ctx.getText());
         //literal constants can be stored as constants in the global scope, indexed by their value
@@ -104,7 +95,7 @@ public class CodeGen extends algBaseVisitor<Symbol>{
     }
 
     //simple_exp --> LITERAL_R
-    public Symbol visitLFloat(alg.LFloatContext ctx)
+    public Symbol visitFloat(alg.FloatContext ctx)
     {
         Symbol s = new Symbol(new Type(Type.PType.FLOAT),ctx.getText(),ctx.getText());
         //literal constants can be stored as constants in the global scope, indexed by their value
@@ -115,7 +106,7 @@ public class CodeGen extends algBaseVisitor<Symbol>{
     }
 
     //simple_exp --> LITERAL_STRING
-    public Symbol visitLString(alg.LStringContext ctx)
+    public Symbol visitString(alg.StringContext ctx)
     {
         String strText = ctx.getText();
         //we need to remove the trailing "" and add the null character
@@ -134,14 +125,14 @@ public class CodeGen extends algBaseVisitor<Symbol>{
     }
 
     //simple_exp --> Null
-    public Symbol visitLNull(alg.LNullContext ctx)
+    public Symbol visitNull(alg.NullContext ctx)
     {
         //returns the special symbol null. This one doesn't even need to be stored in the symbol table
         return new Symbol(new Type(true, Type.PType.VOID),"null","null");
     }
 
-    //simple_exp --> (TRUE|FALSE)
-    public Symbol visitLBoolean(alg.LBooleanContext ctx)
+    //simple_exp --> (TRUE)
+    public Symbol visitTrue(alg.TrueContext ctx)
     {
         Symbol s = new Symbol(new Type(Type.PType.BOOLEAN),ctx.getText(),ctx.getText());
         //literal constants can be stored as constants in the global scope, indexed by their value
@@ -151,10 +142,22 @@ public class CodeGen extends algBaseVisitor<Symbol>{
         return globalScope.resolve(s.name);
     }
 
+    //simple_exp --> (FALSE)
+    public Symbol visitFalse(alg.FalseContext ctx)
+    {
+        Symbol s = new Symbol(new Type(Type.PType.BOOLEAN),ctx.getText(),ctx.getText());
+        //literal constants can be stored as constants in the global scope, indexed by their value
+        this.globalScope.define(s);
+        //we need to resolve, because it might be the case that the same constant was already declared, and in this case
+        //we return the previously created symbol instead of a new one
+        return globalScope.resolve(s.name);
+    }
+
+
     //simple_exp --> IDENT
     public Symbol visitVar(alg.VarContext ctx)
     {
-        return this.scopes.get(ctx).resolve(ctx.IDENT().getText());
+        return this.scopes.get(ctx).resolve(ctx.INDENT().getText());
     }
 
     public Symbol visitSimpleExp(alg.SimpleExpContext ctx)
@@ -170,14 +173,6 @@ public class CodeGen extends algBaseVisitor<Symbol>{
     //Task T2
     //exp --> QMark left_side #PointerExtract
     public Symbol visitPointerExtract(alg.PointerExtractContext ctx)
-    {
-        //TODO
-    }
-
-    //Task T3
-    //exp --> exp LRECT exp RRECT #PointerAccess
-    //usado no lado direito de uma atribuição, ou como uma expressão sózinha.
-    public Symbol visitPointerAccess(alg.PointerAccessContext ctx)
     {
         //TODO
     }
@@ -265,32 +260,6 @@ public class CodeGen extends algBaseVisitor<Symbol>{
     }
 
 
-
-    //function declarations
-    //f_decl --> (type|VOID) IDENT LPAR formalParameters? RPAR body #FunctionDecl
-    //Task T4
-    public Symbol visitFunctionDecl(alg.FunctionDeclContext ctx)
-    {
-        //TODO
-    }
-
-
-
-    //f_decl --> INT ALG LPAR INT IDENT VIR LTHAN STRING GTHAN IDENT RPAR body #MainDecl;
-    //Task T4
-    public Symbol visitMainDecl(alg.MainDeclContext ctx)
-    {
-        //TODO
-    }
-
-    //function invocations
-    //f_inv --> IDENT LPAR (exp (VIR exp)*)? RPAR     #FCall
-    //Task T5
-    public Symbol visitFCall(alg.FCallContext ctx)
-    {
-        //TODO
-    }
-
     //instructions
     //inst --> exp PVIR #ExpInst
     public Symbol visitExpInst(alg.ExpInstContext ctx)
@@ -299,20 +268,6 @@ public class CodeGen extends algBaseVisitor<Symbol>{
         visit(ctx.exp());
         //It is not mandatory to return any symbol in an instruction
         return null;
-    }
-
-    //Task T6
-    //inst --> left_side EQUAL exp PVIR;
-    public Symbol visitAssign(alg.AssignContext ctx)
-    {
-        //TODO
-    }
-
-    //T6
-    //left_side --> IDENT
-    public Symbol visitLeftVar(alg.LeftVarContext ctx)
-    {
-        //TODO
     }
 
 
@@ -341,27 +296,6 @@ public class CodeGen extends algBaseVisitor<Symbol>{
         return null;
     }
 
-    //inst --> WHILE exp DO inst (FINALLY inst)? #While
-    //Task T8
-    public Symbol visitWhile(alg.WhileContext ctx)
-    {
-        //TODO
-    }
-
-    //Task T8
-    //ctrl_inst --> LEAVE #Leave
-    public Symbol visitLeave(alg.LeaveContext ctx)
-    {
-        //TODO
-    }
-
-    //Task T8
-    //ctrl_inst --> RESTART #Restart
-    public Symbol visitRestart(alg.RestartContext ctx)
-    {
-        //TODO
-    }
-
     public Symbol visitReturn(alg.ReturnContext ctx)
     {
         emit("return");
@@ -374,7 +308,5 @@ public class CodeGen extends algBaseVisitor<Symbol>{
         emit("return " + returnValue.name);
         return null;
     }
-
-
 
 }
